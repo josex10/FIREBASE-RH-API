3bbf4da612dac80b62a8ead130bae5fef8974637
FF_400JoseBadilla <ff_400jose@FF-400JoseBadillas-MacBook-Pro.local>
2022-09-20 23:46:38 -0600

    Add the validation to all the endpoints and also add the string length validation

--- functions/src/router_v1/role.router.ts
+++ functions/src/router_v1/role.router.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, RequestHandler, Response, Router } from 'express'
 import { RoleController } from '../controllers/role.controller'
-import { createRoleValidationRules, getRoleByIDValidationRules } from '../shared/validators/role.validator'
-import { executeValidator } from '../shared/validators/general.validator'
+import { roleValidationRules } from '../shared/validators/role.validator'
+import { executeValidator, uidValidationRules } from '../shared/validators/general.validator'
 
 export class RoleRouter {
   public router: Router
@@ -23,7 +23,7 @@ export class RoleRouter {
     }) as RequestHandler)
 
     this.router.get('/role/:uid',
-      getRoleByIDValidationRules(),
+      uidValidationRules(),
       (req: Request, res: Response, next: NextFunction): void => {
         executeValidator(req, res, next)
       },
@@ -32,7 +32,7 @@ export class RoleRouter {
       }) as RequestHandler)
 
     this.router.post('/role',
-      createRoleValidationRules(),
+      roleValidationRules(),
       (req: Request, res: Response, next: NextFunction): void => {
         executeValidator(req, res, next)
       },
@@ -40,16 +40,32 @@ export class RoleRouter {
         await this.roleController.createRole(req, res)
       }) as RequestHandler)
 
-    this.router.put('/role/:uid', (async (req, res) => {
-      await this.roleController.updateRole(req, res)
+    this.router.put('/role/:uid', 
+      uidValidationRules(),
+      roleValidationRules(),
+      (req: Request, res: Response, next: NextFunction): void => {
+        executeValidator(req, res, next)
+      },
+      (async (req, res) => {
+        await this.roleController.updateRole(req, res)
     }) as RequestHandler)
 
-    this.router.delete('/role/:uid', (async (req, res) => {
-      await this.roleController.deleteRole(req, res)
-    }) as RequestHandler)
+    this.router.delete('/role/:uid', 
+      uidValidationRules(),
+      (req: Request, res: Response, next: NextFunction): void => {
+        executeValidator(req, res, next)
+      },
+      ( async (req, res) => {
+        await this.roleController.deleteRole(req, res)
+      }) as RequestHandler)
 
-    this.router.put('/role/activate/:uid', (async (req, res) => {
-      await this.roleController.activateRole(req, res)
+    this.router.put('/role/activate/:uid', 
+      uidValidationRules(),
+      (req: Request, res: Response, next: NextFunction): void => {
+        executeValidator(req, res, next)
+      },
+      ( async (req, res) => {
+        await this.roleController.activateRole(req, res)
     }) as RequestHandler)
   }
 }
